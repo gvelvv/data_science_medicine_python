5aeb625a6537db275f5ae618354c38d8912f98d5
gvelvv <tyazhelnikov@biocad.ru>
2021-06-12 20:00:01 +0300

    hw3

---
+++ lesson_3/hw3_q2.py
@@ -0,0 +1,3 @@
+def info(**kwargs):
+    return kwargs
+print(info(name="Ivan", surname="Ivanov", birth_year=1965, city="SPb", email="ex@info.ru", phone=298537623))
---
+++ lesson_3/hw3_q6.py
@@ -0,0 +1,7 @@
+def int_func(w):
+    w = w.title()
+    return w
+word = input('Input word: ')
+print(int_func(word))
+words = input('Input words: ')
+print(int_func(words))
